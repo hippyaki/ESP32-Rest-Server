81af3f07168e4c50f6158069553bdca592ba323b
Akshayan Sinha <akshayan.sinha@gmail.com>
2022-04-16 13:35:08 +0530

    Update device.js

--- device.js
+++ device.js
@@ -4,9 +4,9 @@ const deviceSchema = new mongoose.Schema({
     name: String,
     data:  { type: Number, set: function (v) { return Math.round(v) ;}},
     status: Boolean
-    });
+    }, { versionKey: false });
 
-deviceSchema.set('toObject', { versionKey: false });
+//deviceSchema.set('toObject', { versionKey: false });
 
 const deviceModel = mongoose.model("Device", deviceSchema);
 
