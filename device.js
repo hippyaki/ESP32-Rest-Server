84e2bb5142db8e2c31f9948023e15b075f63a617
Akshayan Sinha <akshayan.sinha@gmail.com>
2022-04-16 14:00:05 +0530

    Update device.js

--- device.js
+++ device.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const deviceSchema = new mongoose.Schema({
     name: String,
-    data:  { type: Number, set: function (v) { return Math.round(v) ;}},
+    data:  mongoose.Decimal128,
     status: Boolean
     }, { versionKey: false });
 
